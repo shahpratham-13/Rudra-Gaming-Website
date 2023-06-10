6cbbf0722564fa01dd2c62c5233c3957fbb461c4
Pratham Shah <shahpratham130@gmail.com>
2023-06-10 08:09:01 +0545

    changed whole code

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+// Dropdown menu JS
 const dropdownMenu = document.querySelector(".dropdown-menu");
 const dropdown = document.querySelector(".dropdown");
-dropdownMenu.addEventListener("click",()=>{
-    dropdown.classList.toggle("toggleDropdown");
-    dropdown.style.opacity = "1";
-    dropdownMenu.children[1].classList.toggle("iconRotate");
-});
+const mousenter = ()=>{
+    dropdown.style.height = "17rem"
+    dropdown.style.opacity = "1"
+    dropdownMenu.children[0].children[1].style.transform = "rotate(180deg)";
+}
+const mouseleave = ()=>{
+        dropdown.style.height = "0"
+    dropdown.style.opacity = "0"
+    dropdownMenu.children[0].children[1].style.transform = "rotate(0deg)";
+}
+dropdownMenu.children[0].addEventListener("mouseenter",mousenter);
+dropdown.addEventListener("mouseenter",mousenter);
+dropdownMenu.children[0].addEventListener("mouseleave",mouseleave);
+dropdown.addEventListener("mouseleave",mouseleave);
